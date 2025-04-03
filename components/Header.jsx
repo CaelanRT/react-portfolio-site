e13a2e7b9558ecc9521106d5fa08314a75495e37
CaelanRT <caelanrt15@gmail.com>
2025-04-03 17:51:18 -0400

    learning React!

---
+++ components/Header.jsx
@@ -0,0 +1,7 @@
+function Header ({ name }) {
+    return (
+        <header className="text-2xl text-red-500">{name} - Software Engineer</header>
+    )
+}
+
+export default Header;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header name="Caelan" />
+    </div>
+  )
+}
+
+export default App;
