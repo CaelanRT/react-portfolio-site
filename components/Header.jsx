11587ed416a893fd19b2152676c4c6849eec664f
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-05-04 17:02:41 -0400

    have a functioning single page app with routing to my pages

--- components/Header.jsx
+++
@@ -1,7 +0,0 @@
-function Header ({ name }) {
-    return (
-        <header className="text-2xl text-red-500">{name} - Software Engineer</header>
-    )
-}
-
-export default Header;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Header from '../components/Header';
+import { Routes, Route } from 'react-router-dom';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
-    <div>
-      <Header name="Caelan" />
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   )
 }
 
